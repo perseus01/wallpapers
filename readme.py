e03360f6be418c1714d639e14853a21a4397e344
perseus <perseus@git.com>
2021-05-08 22:26:22 +0530

    reset

---
+++ readme.py
@@ -0,0 +1,23 @@
+# Python script to generate README.md file for https://github.com/perseus01/wallpapers
+
+import os
+import re
+
+head = """# Wallpapers
+### Preview:
+\n
+"""
+
+f = open('README.md', 'w')
+f.write(head)
+
+# Sorted (in ascending order) list of all wallpapers in the directory.
+files_list = os.listdir()
+wallpapers_list = sorted(list(filter(re.compile('[0-9]{4}\.(jpg|png|jpeg)').match, files_list)))
+
+# Generate wallpaper previews in README.md
+f.write('<div text-align="center">\n')
+for name in wallpapers_list:
+    f.write(f'<img src="https://raw.githubusercontent.com/perseus01/wallpapers/main/{name}" alt="{name[:4]}"> <p font-size="1.5em">{name[:4]}</p>\n')
+f.write('</div>')
+f.close()
