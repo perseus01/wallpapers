a51b769a2b78d556a9507ca3fcfd90d5c7625f9d
perseus <perseus@git.com>
2021-05-08 22:38:16 +0530

    fix preview alignment (image center)
    preview alignment

--- readme.py
+++ readme.py
@@ -16,8 +16,6 @@ files_list = os.listdir()
 wallpapers_list = sorted(list(filter(re.compile('[0-9]{4}\.(jpg|png|jpeg)').match, files_list)))
 
 # Generate wallpaper previews in README.md
-f.write('<div text-align="center">\n')
 for name in wallpapers_list:
-    f.write(f'<img width="50%" src="https://raw.githubusercontent.com/perseus01/wallpapers/main/{name}" alt="{name[:4]}"> <p font-size="1.5em">{name[:4]}</p>\n')
-f.write('</div>')
+    f.write(f'<p font-size="1.5em" align="center"><img width="50%" src="https://raw.githubusercontent.com/perseus01/wallpapers/main/{name}" alt="{name[:4]}"><br>{name[:4]}</p>\n')
 f.close()
