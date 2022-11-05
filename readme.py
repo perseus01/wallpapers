d2e1537e5e4593b7252099a5711dcc63d4947fa8
Divyang <Divyang@git.com>
2022-11-04 23:51:34 -0400

    Changed folder structure and removed some wallpapers

--- readme.py
+++ readme.py
@@ -4,6 +4,9 @@ import os
 import re
 
 head = """# Wallpapers
+
+### Click on a preview to download the wallpaper.
+
 ### Preview:
 \n
 """
@@ -11,11 +14,12 @@ head = """# Wallpapers
 f = open('README.md', 'w')
 f.write(head)
 
-# Sorted (in ascending order) list of all wallpapers in the directory.
-files_list = os.listdir()
+# Sorted (in ascending order) list of all wallpapers in the wps directory.
+cur_dir = os.getcwd()
+files_list = os.listdir(f'{cur_dir}/wps')
 wallpapers_list = sorted(list(filter(re.compile('[0-9]{4}\.(jpg|png|jpeg)').match, files_list)))
 
 # Generate wallpaper previews in README.md
 for name in wallpapers_list:
-    f.write(f'<p font-size="1.5em" align="center"><img width="75%" src="https://raw.githubusercontent.com/perseus01/wallpapers/main/{name}" alt="{name[:4]}"><br>{name[:4]}</p>\n')
+    f.write(f'<p font-size="1.5em" align="center"><img width="75%" src="https://raw.githubusercontent.com/perseus01/wallpapers/main/wps/{name}" alt="{name[:4]}"><br>{name[:4]}</p>\n')
 f.close()
