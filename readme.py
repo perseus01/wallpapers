12088c9d538c669ee4707ae22142c495f61b3af6
Divyang <8656786+perseus01@users.noreply.github.com>
2024-12-06 22:38:43 -0500

    chore: apply pep8 formatting to readme.py

--- readme.py
+++ readme.py
@@ -12,15 +12,19 @@ The following wallpapers are fetched from various sources across the internet. I
 \n
 """
 
-f = open('README.md', 'w')
+f = open("README.md", "w")
 f.write(head)
 
 # Sorted (in ascending order) list of all wallpapers in the wps directory.
 cur_dir = os.getcwd()
-files_list = os.listdir(f'{cur_dir}/wps')
-wallpapers_list = sorted(list(filter(re.compile('[0-9]{4}\.(jpg|png|jpeg)').match, files_list)))
+files_list = os.listdir(f"{cur_dir}/wps")
+wallpapers_list = sorted(
+    list(filter(re.compile("[0-9]{4}\.(jpg|png|jpeg)").match, files_list))
+)
 
 # Generate wallpaper previews in README.md
 for name in wallpapers_list:
-    f.write(f'<p font-size="1.5em" align="center"><img width="75%" src="https://raw.githubusercontent.com/perseus01/wallpapers/main/wps/{name}" alt="{name[:4]}"><br>{name[:4]}</p>\n')
+    f.write(
+        f'<p font-size="1.5em" align="center"><img width="75%" src="https://raw.githubusercontent.com/perseus01/wallpapers/main/wps/{name}" alt="{name[:4]}"><br>{name[:4]}</p>\n'
+    )
 f.close()
